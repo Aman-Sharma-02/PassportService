798128365b621425635855cc37c97522fda06b37
Aman Sharma <Aman.Sharma@Triconinfotech.com>
2021-10-19 10:02:03 +0530

    Delete PassportServices directory

--- PassportServices/src/main/java/com/begin/passport1/dao/PersonDao.java
+++
@@ -1,8 +0,0 @@
-package com.begin.passport1.dao;
-
-import com.begin.passport1.Entities.Person;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PersonDao extends JpaRepository<Person, Integer> {
-
-}
