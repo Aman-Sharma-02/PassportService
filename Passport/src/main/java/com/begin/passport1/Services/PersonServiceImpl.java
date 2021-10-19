d868cfdf5aca4f3978248902bd19103415a9b136
Aman Sharma <Aman.Sharma@Triconinfotech.com>
2021-10-19 10:00:11 +0530

    Update PersonServiceImpl.java

--- Passport/src/main/java/com/begin/passport1/Services/PersonServiceImpl.java
+++ Passport/src/main/java/com/begin/passport1/Services/PersonServiceImpl.java
@@ -19,41 +19,52 @@ public class PersonServiceImpl implements PersonService {
     }
 
     @Override
-    public List<Person> getPersons()
+    public List<Person> getListOfIssueRequest()
     {
-
         return personDao.findAll();
     }
 
     @Override
-    public Optional<Person> getPerson(int aadharId) {
-        //return personDao.getOne(aadharId);
-        Optional<Person> p= personDao.findById( aadharId);
-        return p;
-
-    }
-   /* public Person getPerson(int aadharId) {
-        //return personDao.getOne(aadharId);
-        Optional<Person> p= personDao.findById( aadharId);
-        return p;
-
-    }*/
-
-    @Override
-    public Person addPerson(Person person) {
-        personDao.save(person);
+    public Person getIssueRequest(int aadharId) {
+        Optional<Person> p = personDao.findById(aadharId);
+        Person person=null;
+       if(p.isPresent()) {
+           person = p.get();
+       }
+        else
+        {
+            throw new RuntimeException("Person record not found for AadharNumber :: "+aadharId);
+        }
         return person;
     }
 
     @Override
-    public Person updatePerson(Person person) {
-        personDao.save(person);
-        return person;
+    public void addIssueRequest(Person person)
+    {
+        int aadhar = person.getAadharNo();
+        String gender = person.getGender().toUpperCase();
+        int aadhar_size = String.valueOf(aadhar).length();
+
+        if(aadhar_size == 4)
+        {
+            if(gender.equals("MALE") || gender.equals("FEMALE"))
+            {
+                this.personDao.save(person);
+            }
+            else
+            {
+                throw new RuntimeException("please enter valid gender (Male or Female) :: "+gender);
+            }
+        }
+        else
+        {
+            throw new RuntimeException("Please enter valid AadharNumber (4 Digit Number) :: "+aadhar);
+        }
     }
 
     @Override
-    public void deletePerson(int parseInt) {
-        Person entity = personDao.getOne(parseInt);
-        personDao.delete(entity);
+    public void deleteRequest(int id) {
+        this.personDao.deleteById(id);
     }
+
 }
