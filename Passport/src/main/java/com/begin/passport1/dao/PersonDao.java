eec00aab5aa46e1e46742a34e287bc67453d69a5
Aman Sharma <Aman.Sharma@Triconinfotech.com>
2021-10-19 10:01:14 +0530

    Update PersonDao.java

--- Passport/src/main/java/com/begin/passport1/dao/PersonDao.java
+++ Passport/src/main/java/com/begin/passport1/dao/PersonDao.java
@@ -2,7 +2,9 @@ package com.begin.passport1.dao;
 
 import com.begin.passport1.Entities.Person;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface PersonDao extends JpaRepository<Person, Integer> {
 
 }
