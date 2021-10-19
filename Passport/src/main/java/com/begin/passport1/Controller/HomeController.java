1eb12f16c9ee134a3320ac5e21e663ca16bb7a94
Aman Sharma <Aman.Sharma@Triconinfotech.com>
2021-10-19 09:48:56 +0530

    Update HomeController.java

--- Passport/src/main/java/com/begin/passport1/Controller/HomeController.java
+++ Passport/src/main/java/com/begin/passport1/Controller/HomeController.java
@@ -6,7 +6,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HomeController {
@@ -15,22 +14,44 @@ public class HomeController {
     private PersonService personService;
 
     @GetMapping
-    public String home(Model model)
+    public String homepage()
     {
-        model.addAttribute("person", new Person());
-        return "apply_passport";
+        return "Homepage";
     }
 
-    @GetMapping("/request_list")
-    public ModelAndView getRequestList() {
-        ModelAndView mav = new ModelAndView("passport_validator");
-        mav.addObject("person", personService.getPersons());
-        return mav;
+    @GetMapping("/ApplyPassport")
+    public String applyPassport(Model model) {
+        Person person = new Person();
+        model.addAttribute("person", person);
+        return "ApplyPassport";
     }
 
-    @PostMapping(value = "/savePerson")
-    public String addPerson() {
-        return "successful";
+    @GetMapping("/PassportIssueRequestList")
+    public String viewData(Model model)
+    {
+        model.addAttribute("people", personService.getListOfIssueRequest());
+        return "ValidationList";
+    }
+
+    @PostMapping("/SaveRequest")
+    public String addIssueRequest(@ModelAttribute("person") Person person) {
+        personService.addIssueRequest(person);
+        return "Homepage";
+    }
+
+    @GetMapping("/ValidateApplication/{id}")
+    public String ValidatePerson(@PathVariable ( value = "id") int id, Model model)
+    {
+        Person person = personService.getIssueRequest(id);
+        model.addAttribute("person",person);
+        return "ValidationPage";
+    }
+
+    @GetMapping("/RejectApplication/{id}")
+    public String RejectApplication(@PathVariable ( value = "id") int id)
+    {
+        this.personService.deleteRequest(id);
+        return "Homepage";
     }
 
 
